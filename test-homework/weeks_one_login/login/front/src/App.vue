125c5bf91d20d59d8a391a59a379a27b9e68e51e
2019997204 <2019997204@qq.com>
2020-12-27 12:32:14 +0800

    add

---
+++ test-homework/weeks_one_login/login/front/src/App.vue
@@ -0,0 +1,120 @@
+<template>
+  <div id="app">
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action="">
+        <div class="layui-form-item">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-inline">
+            <input
+              type="text"
+              name="name"
+              v-model="name"
+              v-validate="'required|email'"
+              placeholder="请输入用户名"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+          <div class="error layui-form-mid">{{errors.first('name')}}</div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-inline">
+            <input
+              type="password"
+              name="title"
+              v-model="password"
+              required
+              lay-verify="required"
+              placeholder="请输入密码"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-inline">
+            <input
+              type="text"
+              name="title"
+              v-model="code"
+              required
+              lay-verify="required"
+              placeholder="请输入验证码"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+          <div class="layui-form-mid layui-word-aux svg" @click="getCaptcha()" v-html="svg"></div>
+        </div>
+        <button type="button" class="layui-btn" @click="checkForm()">立即登录</button>
+        <a href="http://www.layui.com" class="imooc-link">忘记密码</a>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      svg: '',
+      name: '',
+      password: '',
+      code: '',
+      errorMsg: []
+    }
+  },
+  mounted () {
+    this.getCaptcha()
+  },
+  methods: {
+    getCaptcha () {
+      axios.get('http://localhost:3000/getCaptcha').then((res) => {
+        if (res.status === 200) {
+          const obj = res.data
+          if (obj.code === 200) {
+            this.svg = obj.data
+          }
+        }
+      })
+    },
+    checkForm () {
+      this.errorMsg = []
+      if (!this.name) {
+        this.errorMsg.push('登录名为空!')
+      }
+      if (!this.password) {
+        this.errorMsg.push('密码为空!')
+      }
+      if (!this.code) {
+        this.errorMsg.push('验证码为空!')
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+#app{
+  background: #f2f2f2;
+}
+.layui-container{
+  background: #fafafa;
+}
+input{
+  width: 190px;
+}
+.imooc-link{
+  margin-left: 10px;
+  &:hover{
+    color: #009688;
+  }
+}
+.svg{
+  position: relative;
+  top: -15px;
+}
+</style>
